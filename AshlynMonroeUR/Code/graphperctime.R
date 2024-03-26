e8e397c294c3f2c79386cbcddef56ea4b53f5fa8
Meghan Slocombe <slocombe.usu@gmail.com>
2024-03-25 18:09:43 -0600

    Updated code for graph of cover over time

--- AshlynMonroeUR/Code/graphperctime.R
+++ AshlynMonroeUR/Code/graphperctime.R
@@ -1,7 +1,24 @@
 #Load packages and data ----
 library(tidyverse)
 
-pct <- read.csv("./Data/clean_perc_covertimeAM.csv", header = TRUE)
+pctdownload <- read.csv("./Data/clean_perc_covertimeAM.csv", header = TRUE)
+
+#Add rows of data for all 0s ----
+#Edit pct to only include plant codes of interest (FL, WL, T, DW) and no PRD
+pctcl <- pctdownload %>% filter(pl_code == "FL" | 
+                          pl_code == "WL" | 
+                          pl_code == "T" | 
+                          pl_code == "DW") %>%
+  filter(loc != "PRD")
+
+pctfill <- crossing(1:6, pctcl$block, pctcl$loc, pctcl$trt, pctcl$pl_code)
+colnames(pctfill) <- c("tms", "block", "loc", "trt", "pl_code")
+
+pct <- pctfill %>%
+  left_join(pctcl, by = c("tms", "block", "loc", "trt", "pl_code")) %>%
+  select(-X, -tank)
+
+pct$pro[is.na(pct$pro)] <- 0
 
 #Create matrix w/ mean and sd for each plant category ----
 #Fineleaf
@@ -10,45 +27,38 @@ unique(pct$tms)
 tms <- seq(1,6)
 fl_perc <- matrix(NA, nrow = 6, ncol=3)
 fl_perc[,1] <- seq(1,6)
-fl_perc [1,2:3] <- 0
 
 colnames(fl_perc) <- c("tms", "mean", "sd")
 
-for(i in 2:length(tms)){
+for(i in 1:length(tms)){
   
   temp <- pct %>% filter(tms == i & trt == "5") %>%
     filter(pl_code == "FL")
   
-  fl_perc[i,2] <- (sum(temp$pro))/20
+  fl_perc[i,2] <- mean(temp$pro)
   fl_perc[i,3] <- sd(temp$pro)
   
 }
 
 #Whorled
-unique (pct$tms)
-
 wl_perc <- matrix(NA, nrow = 6, ncol=3)
 wl_perc[,1] <- seq(1,6)
-wl_perc [1,2:3] <- 0
 
 colnames(wl_perc) <- c("tms", "mean", "sd")
 
-for(i in 2:length(tms)){
+for(i in 1:length(tms)){
   
   temp <- pct %>% filter(tms == i & trt == "5") %>%
     filter(pl_code == "WL")
   
-  wl_perc[i,2] <- (sum(temp$pro))/20
+  wl_perc[i,2] <- mean(temp$pro)
   wl_perc[i,3] <- sd(temp$pro)
   
 }
 
-#Only one observation of whorled for timestep 2, no sd calculated
-
 #Terrestrial
 t_perc <- matrix(NA, nrow = 6, ncol=3)
 t_perc[,1] <- seq(1,6)
-t_perc [1,2:3] <- 0
 
 colnames(t_perc) <- c("tms", "mean", "sd")
 
